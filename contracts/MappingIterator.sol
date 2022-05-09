e410a8ca09d1b77c09817f87a33547ec7840c625
Izzuddin <izzuddin.y@gmail.com>
2022-05-09 21:40:37 +0800

    Mapping Iterator

---
+++ contracts/MappingIterator.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract MappingIterator{
+    mapping(address=>uint) public values;
+    address[] public arr;
+    function pay(uint _value) public {
+        values[msg.sender] = _value;
+        arr.push(msg.sender);
+    }
+
+    function returnArray() public view returns(address[] memory){
+        return arr;
+    }
+}
